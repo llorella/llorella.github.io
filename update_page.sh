fa1528bcebdc58dc17a29ba14c9f0f1b0063ea91
llorella <lucianolauro77@gmail.com>
2024-04-06 00:29:47 -0700

    Site Updated on 2024-04-06 00:29:47

--- update_page.sh
+++ update_page.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 PROJECT_DIR="$HOME/llorella.github.io"
+HTML_FILE="$PROJECT_DIR/index.html"
+TEMP_FILE="$PROJECT_DIR/index.html.tmp"
 
 # user runs LLT=1 ./update_page.sh
 if [ "$LLT" -eq 1 ]; then
-    llt --non_interactive -l improve_homepage_demo.ll -f $PROJECT_DIR/index.html \ 
-    -p "Change the main colors to black and white" \
-    | md -n 1 -f "index.html" > $PROJECT_DIR/index.html
+    llt --non_interactive -l improve_homepage.ll -f $HTML_FILE \ 
+    -p "Add syntax highlighting for content of bash-script-content component." \
+    | md > $HTML_FILE
 fi
 # Load the current state of the page and running ll context with specific system instructions non-interactively. 
 # Pipe output to md,  extract the first code block with heading index.html, and write output to index.html.
 
-HTML_FILE="$PROJECT_DIR/index.html"
-TEMP_FILE="$PROJECT_DIR/index.html.tmp"
-
 CURRENT_DATE=$(date '+%Y-%m-%d %H:%M:%S')
-
 sed "s/Last updated on: .*/Last updated on: $CURRENT_DATE<\/p>/" $HTML_FILE > $TEMP_FILE
 mv $TEMP_FILE $HTML_FILE
 
